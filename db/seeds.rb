c44fa8fca0fd6cc834d4c650ec17407666aa85e9
TyneBloomfield <cat.is.fite@gmail.com>
2016-01-29 18:06:06 +0000

    data entry template in seed file

---
+++ db/seeds.rb
@@ -0,0 +1,9 @@
+# Template to create new entries for messages:
+# --------------------------
+# Message.create(
+#     text: "",
+#     spice: true or false,
+#     section: ""
+#   );
+# --------------------------
+
