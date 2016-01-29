04058b6fa3d196d322705a7a9f0de564593b9f45
jaredpon <jpon44@gmail.com>
2016-01-29 19:35:27 +0000

    refactored the core feature

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,37 +1,12 @@
 require 'twilio-ruby' 
  
-
 class Message < ActiveRecord::Base
   has_many :sent_messages
 
-  ACCOUNT_SID = 'AC01b6404ed79574caed356c0e13d652ed' 
-  AUTH_TOKEN = 'a19da2c5470eea4fa932ae69c57ebb71'
-
-  def self.intro_generator(spicy)
-    where(spice: spicy, section: "Intro").order("RANDOM()").first
-  end
-
-  def self.body_generator(spicy)
-    where(spice: spicy, section: "Body").order("RANDOM()").first
-  end
-
-  def self.ending_generator(spicy)
-    where(spice: spicy, section: "Ending").order("RANDOM()").first
-  end
-
-  def self.message_maker(recipient, sender, intro, body, ending)
-    "Dear #{recipient} " + "#{intro} " + "#{body} " + "#{ending} " + "from #{sender}."
-  end
-
-  def self.send_sms(phone_number, alert_message) 
-    @twilio_number = +12048185591
-    @client = Twilio::REST::Client.new ACCOUNT_SID, AUTH_TOKEN
-
-    message = @client.account.messages.create(
-      :from => @twilio_number,
-      :to => phone_number,
-      :body => alert_message
-    )
-    puts message.to
+  def self.generator(spicy)
+    @message_array = []
+    @message_array << where(spice: spicy, section: "Intro").order("RANDOM()").first
+    @message_array << where(spice: spicy, section: "Body").order("RANDOM()").first
+    @message_array << where(spice: spicy, section: "Ending").order("RANDOM()").first
   end
 end
--- app/models/sent_message.rb
+++ app/models/sent_message.rb
@@ -1,8 +1,30 @@
 class SentMessage < ActiveRecord::Base
-belongs_to :message
+  belongs_to :message
 
-validates :intro, presence: true
-validates :body, presence: true
-validates :ending, presence: true
+  validates :intro, presence: true
+  validates :body, presence: true
+  validates :ending, presence: true
 
+  ACCOUNT_SID = 'AC01b6404ed79574caed356c0e13d652ed' 
+  AUTH_TOKEN = 'a19da2c5470eea4fa932ae69c57ebb71'
+
+  def message_maker(recipient, sender, intro, body, ending)
+    "Dear #{recipient} " + "#{intro} " + "#{body} " + "#{ending} " + "from #{sender}."
+  end
+
+  def send_sms(phone_number, alert_message) 
+    @twilio_number = +12048185591
+    @client = Twilio::REST::Client.new ACCOUNT_SID, AUTH_TOKEN
+
+    message = @client.account.messages.create(
+      :from => @twilio_number,
+      :to => phone_number,
+      :body => alert_message
+    )
+    puts message.to
+  end
+
+  def self.recent_messages
+    
+  end
 end
